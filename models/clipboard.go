0e75b54f8af677800391466de2831264e10cf5a6
enkaypeter <enkaypeter@gmail.com>
2025-07-20 23:51:48 +0100

    add clipboard entry dtos

--- models/clipboard.go
+++ models/clipboard.go
@@ -7,12 +7,12 @@ import (
 )
 
 type ClipboardEntry struct {
-	ID        primitive.ObjectID `bson:"_id,omitempty" json:"id"`
-	Type     string    `json:"type"`         
-	Filename string    `json:"filename,omitempty"`
-	Content  string    `json:"content"`       
-	Timestamp time.Time `json:"timestamp"`
-	Filepath  string    `json:"filepath,omitempty"`
-	UserId		primitive.ObjectID    `json:"user_id" bson:"user_id"`
-	Pinned    bool      `json:"pinned" bson:"pinned,omitempty"` // Added Pinned field
+	ID            primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Type          string             `json:"type"`
+	Filename      string             `json:"filename,omitempty"`
+	Content       string             `json:"content"`
+	Timestamp     time.Time          `json:"timestamp"`
+	Filepath      string             `json:"filepath,omitempty"`
+	UserId        primitive.ObjectID `json:"user_id" bson:"user_id"`
+	Pinned        bool               `json:"pinned" bson:"pinned,omitempty"`
 }
